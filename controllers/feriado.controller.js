935aa0231abae6aae22627ebef1c2adb04a0957d
DESKTOP-EVKA3QB\maleb <theblackrecords11@gmail.com>
2020-10-30 16:11:44 -0300

    feriados

--- controllers/feriado.controller.js
+++ controllers/feriado.controller.js
@@ -22,6 +22,41 @@ exports.feriado_nuevo = function (req, res) {
         })
 };
 
+exports.feriado_detalle = function (req, res) {
+
+    Feriado.findById(req.body.id, function (err, fer) {
+        if (err) {
+        	console.log(err);
+	            res.json({data:'Error el feriado no existe'});
+        }
+        res.json({feriado: fer});
+    })
+};
+
+exports.feriado_listado = function (req, res) {
+
+    Feriado.find({}, function(err, feriados) {
+      if (err) {
+        console.log(err);
+            res.json({data:'Error no hay feriados'});
+      }
+         res.json({data:feriados});
+      });
+};
+
+exports.feriado_delete = function(req,res){
+
+    Feriado.findByIdAndRemove(req.body.id,function(err,fer){
+      if(err){
+        console.log(err);
+        res.json({data:'Error el feriado no existe'});
+      }
+      res.json({data:'Eliminado con exito'});
+    })
+
+};
+
+
 /*
 var f = new Date();
 document.write(f.getDate() + "/" + (f.getMonth() +1) + "/" + f.getFullYear());
--- routes/feriado.route.js
+++ routes/feriado.route.js
@@ -9,4 +9,10 @@ const feriado_controller = require('../controllers/feriado.controller');
 
 router.post('/nuevo', feriado_controller.feriado_nuevo);
 
+router.post('/detalle', feriado_controller.feriado_detalle);
+
+router.post('/listado', feriado_controller.feriado_listado);
+
+router.post('/delete', feriado_controller.feriado_delete);
+
 module.exports = router;
